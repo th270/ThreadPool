71da12388105143066879396ea9753677f5a8d01
th270 <448725485@qq.com>
2018-08-29 10:40:09 +0800

    代码提交人:汤辉 功能点: java 线程池 来源项目编号/版本简称: 问题分类:代码规范 功能说明/问题现象:java 线程池 方案/解决办法:无 是否完成自测:是 有无兼容性问题:无 有无配合更新模块:无 报告相关路径:不涉及 建议:无 代码审核人:汤辉 文件夹或者文件提交数量: 合并的来源:无

---
+++ src/com/th/threadPool/NewSingleThreadExecutor.java
@@ -0,0 +1,46 @@
+package com.th.threadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Administrator on 2018/6/6.
+ * newSingleThreadExecutor 创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，
+ * 保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行。
+ */
+public class NewSingleThreadExecutor {
+    public static void main(String[] args) {
+
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            /*singleThreadExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("newSingleThreadExecutor: " + index);
+                        Thread.sleep(2*1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });*/
+
+
+            singleThreadExecutor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("newSingleThreadExecutor: " + index);
+                        Thread.sleep(2*1000);
+                    } catch (Exception e) {
+                    	e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        singleThreadExecutor.shutdown();
+    }
+
+}
